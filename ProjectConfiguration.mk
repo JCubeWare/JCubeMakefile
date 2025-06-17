a035b5b0e1acc6437aee95f10ab83b8af344706c
JCubeWare <jcubeware@gmail.com>
2025-06-17 18:41:19 +0200

    JCubeMakefile | Initial commit (v0.0.1)

---
+++ ProjectConfiguration.mk
@@ -0,0 +1,44 @@
+           #################################
+          ###                          #####
+        ###     ######################### ##
+      ###     ###                   ##### ##
+    ####     ########################  ## ##
+  ####                          ###    ## ##
+####                          ####     ## ##
+################################ #     ## ##
+##                     #     ##  #  ## ## ##
+##                     #     ##  #  ## ## ##
+##                     #     ##  #  ##### ##
+##                     #     ##  # ###### ##
+##                     #     ##  # #  ### ##
+##                     #     ##  ###  ### ##
+##                     #     ##  ##    ## ##
+##########             #     ##  ##    #  ##
+##       #             #     ##  ##      ###
+##       #             #     ##  #      ### 
+##       #             #     ##       ###   
+##       ###############     ##     ####    
+##                           ##   ####      
+##                           ## ####        
+##                           #####          
+################################            
+
+############# [J][C]ube[W]are ##############
+#   Be responsible. Code for the future.   #
+#         (c) 2025-2025, JCubeWare         #
+############################################
+
+##====[ PROJECT SETTINGS ]====##
+
+#JCUBEMAKE.PROJECT.
+ JCUBEMAKE.PROJECT.NAME = TestProject
+ #JCUBEMAKE.PROJECT.VERSION.
+  JCUBEMAKE.PROJECT.VERSION.MAJOR = 0
+  JCUBEMAKE.PROJECT.VERSION.MINOR = 0
+  JCUBEMAKE.PROJECT.VERSION.REVISION = 1
+ # Types: LIBRARY, EXECUTABLE
+ JCUBEMAKE.PROJECT.TYPE = LIBRARY
+ # Targets: Win32, Win64, Linux, JCubeOS
+ JCUBEMAKE.PROJECT.TARGETS = Win32 Win64 Linux JCubeOS
+ # Libs that will be used
+ JCUBEMAKE.PROJECT.LIBS = -lTestLib_1.0.0
